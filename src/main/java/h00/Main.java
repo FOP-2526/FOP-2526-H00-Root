b63ae38e6aaf0589fda442594cff8130774e24b5
Dewei Zhu <Dewei.zhu@stud.tu-darmstadt.de>
2025-04-30 22:42:39 +0200

    setupWorld Methode und Todo H0.1

--- src/main/java/h00/Main.java
+++ src/main/java/h00/Main.java
@@ -1,12 +1,22 @@
 package h00;
 
-import fopbot.Robot;
-import fopbot.World;
+import fopbot.*;
 
 /**
  * Main entry point in executing the program.
  */
 public class Main {
+
+    /**
+     * The subtask to run.
+     */
+    public static int runToSubtask = -1;
+
+    /**
+     * The world speed in milliseconds.
+     */
+    public static int delay = 300;
+
 
     /**
      * Main entry point in executing the program.
@@ -15,19 +25,29 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        // This sets up the FOPBot World with a 4x4 grid. (DO NOT TOUCH)
+        setupWorld();
+
+        // TODO: H0.1 - Initializing FOPBot and move
+        Robot robot = new Robot(0,0, Direction.UP,4, RobotFamily.SQUARE_BLUE);
+        robot.move();
+        robot.putCoin();
+
+    }
 
 
-        System.out.println("Hello World!");
+    public static void setupWorld() {
+        // variable representing width/size of world
+        final int worldSize = 4;
 
+        // setting world size symmetrical, meaning height = width
+        World.setSize(worldSize, worldSize);
+
+        // speed of how fast the world gets refreshed (e.g. how fast the robot(s) act)
+        // the lower the number, the faster the refresh
+        World.setDelay(delay);
+
+        // make it possible to see the world window
         World.setVisible(true);
-
-        Robot r = new Robot(1,2);
-        r.move();
-        r.putCoin();
-
-
-
-        World.setVisible(false);
-
     }
 }
