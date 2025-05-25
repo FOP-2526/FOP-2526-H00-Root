dc64f1f660fc60e96c60ef051705be6e5d7182fb
Dewei Zhu <Dewei.zhu@stud.tu-darmstadt.de>
2025-05-25 19:14:32 +0200

    rubric erstellt

---
+++ src/graderPrivate/java/h00/H00_RubricProvider.java
@@ -0,0 +1,39 @@
+package h00;
+
+import org.sourcegrade.jagr.api.rubric.Criterion;
+import org.sourcegrade.jagr.api.rubric.JUnitTestRef;
+import org.sourcegrade.jagr.api.rubric.Rubric;
+import org.sourcegrade.jagr.api.rubric.RubricProvider;
+import org.tudalgo.algoutils.tutor.general.json.JsonParameterSet;
+
+import static org.tudalgo.algoutils.tutor.general.jagr.RubricUtils.criterion;
+
+
+public class H00_RubricProvider implements RubricProvider {
+
+    public static final Rubric RUBRIC = Rubric.builder()
+        .title("H00 | Erste Schritte mit Java & FopBot")
+        .addChildCriteria(
+            Criterion.builder()
+            .shortDescription("H0.1 | Matrikelnummer in Moodle")
+                .minPoints(0)
+                .maxPoints(1)
+            .build(),
+            Criterion.builder()
+                .shortDescription("H0.4 | Erste Programm mit dem FopBot")
+                .addChildCriteria(
+                    criterion(
+                        "Der Konstruktor von Robot wurde mit korrekten Parametern aufgerufen."
+                    )
+                )
+                .minPoints(0)
+                .maxPoints(1)
+                .build()
+
+        ).build();
+
+    @Override
+    public Rubric getRubric() {
+        return null;
+    }
+}
